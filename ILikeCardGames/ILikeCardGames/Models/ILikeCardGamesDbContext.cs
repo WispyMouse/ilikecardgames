58964858c94a5efbfa60f5386e1bbde81686341f
Mint <wispymouse@gmail.com>
2023-07-20 20:07:38 -0700

    Initial Account Information Entity Framework
    This shows proof of concept of connecting to the database using Entity Framework.

---
+++ ILikeCardGames/ILikeCardGames/Models/ILikeCardGamesDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using ILikeCardGames.Models.Account;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace ILikeCardGames.Models
+{
+    public class ILikeCardGamesDbContext : DbContext
+    {
+        public ILikeCardGamesDbContext(DbContextOptions<ILikeCardGamesDbContext> context) : base(context)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString: new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetConnectionString("ILCGPrimary"));
+        }
+
+        public DbSet<ILikeCardGames.Models.Account.Account> Accounts { get; set; }
+        public DbSet<AccountLoginInformation> AccountLoginInformations { get; set; }
+    }
+}
